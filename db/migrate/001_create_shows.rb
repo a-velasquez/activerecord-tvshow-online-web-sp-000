1535241e1af395dc12e5b093da5e5d773f25d984
Alex Velasquez <jtinnesco@gmail.com>
2019-12-27 03:56:36 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -5,24 +5,8 @@ class CreateMovies < ActiveRecord::Migration[5.2]
       t.string  :name
       t.string  :network
       t.string  :day
-      t.integer :rating 
+      t.integer :rating
     end
   end
 
 end
-
-
-
-
-# class CreateMovies < ActiveRecord::Migration[5.2]
-#   def change
-#     create_table :movies do |t|
-#       t.string :title
-#       t.integer :release_date
-#       t.string :director
-#       t.string :lead
-#       t.boolean :in_theaters
-#     end
-#   end
-#
-# end
